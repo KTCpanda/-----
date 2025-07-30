f84e72eca7595955ecfeb0c3812ae375070b905e
KTCpanda <ktc24A31A0006@edu.kyoto-tech.ac.jp>
2025-07-30 16:51:09 +0900

    何度も変更しました

--- review_site/review_site/urls.py
+++ review_site/review_site/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+
+# --- 画像表示のために必須のimport ---
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,6 +10,9 @@ urlpatterns = [
     path('', include('reviews.urls')),
 ]
 
-# 開発環境でメディアファイル（アップロードされた画像）を提供するための設定
+# --- 画像表示のために必須の設定 ---
+# 開発サーバーで動かしている時(DEBUG=True)にのみ、この設定を有効にする
 if settings.DEBUG:
+    # /media/ というURLでリクエストが来た時に、
+    # MEDIA_ROOT (mediaフォルダ) の中からファイルを探すように道案内を追加する
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
